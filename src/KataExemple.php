9ca3d83f73c590916cfacdfb458557b3822a63bf
Laurent Aubertin <2binfree@gmail.com>
2017-03-28 08:57:39 +0200

    add kata strcalc

--- src/KataExemple.php
+++ src/KataExemple.php
@@ -6,18 +6,10 @@ class KataExemple
 {
     public static function action($value)
     {
-       $result = [0, 0];
-       foreach ($value as $number){
-           if($number > 0){
-               $result[0] += $number;
-           }
-           else {
-               $result[1] += $number;
-           }
-       }
-       return $result;
+        return true;
     }
 }
+
 
 
 
--- tests/KataExempleTest.php
+++ tests/KataExempleTest.php
@@ -4,19 +4,8 @@ namespace wcs\tests;
 
 class KataExempleTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFizz() {
-        $this->assertEquals(["positive" => 0, "negative" => -13] , \wcs\KataExemple::action([-3, -5, -1, -1, -2, -1]));
+    public function test1() {
+        $this->assertEquals(true, \wcs\KataExemple::action("value"));
     }
 
-    public function testFizz2() {
-        $this->assertEquals(["positive" => 13, "negative" => 0] , \wcs\KataExemple::action([3, 5, 1, 1, 2, 1]));
-    }
-
-    public function testFizz3() {
-        $this->assertEquals(["positive" => 8, "negative" => -5] , \wcs\KataExemple::action([-3, 5, 1, 1, -2, 1]));
-    }
-
-    public function testFizz4() {
-        $this->assertEquals(["positive" => 10,  "negative" => -3] , \wcs\KataExemple::action([3, 5, 1, -1, -2, 1]));
-    }
 }
